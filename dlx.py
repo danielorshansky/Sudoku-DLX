7c68748193daedac31df94e31152605e567b2b46
Daniel Orshansky <danielorshansky@gmail.com>
2021-04-28 07:37:08 -0500

    Fixed cover and uncover methods and began search function

--- dlx.py
+++ dlx.py
@@ -6,33 +6,57 @@ class Node: # a node in a circular quadruply linked list
         self.up = self.down = self
         self.column = self.row = None
         self.header = header if header else self
+        self.row = None
 
 class Header(Node): # a header node for each column
     def __init__(self):
         super().__init__()
         self.count = 0 # the number of nodes with a value of 1
     
-    def cover(self):
+    def cover(self): # cover a column
         self.left.right = self.right
         self.right.left = self.left
-        node = self.down
-        while node != self:
-            node.left.right = node.right
-            node.right.left = node.left
-            node = node.down
+        vertical_node = self.down
+        while vertical_node != self:
+            horizontal_node = vertical_node.right
+            while horizontal_node != vertical_node:
+                horizontal_node.down.up = horizontal_node.up
+                horizontal_node.up.down = horizontal_node.down
+                horizontal_node.header.count -= 1
+                horizontal_node = horizontal_node.right
+            vertical_node = vertical_node.down
     
-    def uncover(self):
+    def uncover(self): # uncover a column
         self.left.right = self
         self.right.left = self
-        node = self.down
-        while node != self:
-            node.left.right = node
-            node.right.left = node
-            node = node.down
+        vertical_node = self.down
+        while vertical_node != self:
+            horizontal_node = vertical_node.right
+            while horizontal_node != vertical_node:
+                horizontal_node.down.up = self
+                horizontal_node.up.down = self
+                horizontal_node.header.count += 1
+                horizontal_node = horizontal_node.right
+            vertical_node = vertical_node.down
 
 class DLX:
     def __init__(self):
         self.root = Header()
+        self.solutions = []
+    
+    def get_minimum(self): # find the column with the minimum 1s
+        header = self.root.right.right
+        minimum = self.root.right.count
+        while header != self.root:
+            if header.count == 0:
+                header.left.right = header.right
+                header.right.left = header.left
+                continue
+            if header.count == 1:
+                return header
+            if header.count < minimum:
+                minimum = header
+            header = header.right
     
     def create_links(board, n): # pass the board and size of n x n sudoku
         box_size = math.sqrt(n)
@@ -55,11 +79,12 @@ class DLX:
                     for index, node in enumerate(nodes):
                         node.right = nodes[index + 1] # link nodes to each other
                         node.left = nodes[index - 1]
-                        node.header.count += 1 # add one to the count of the header
                         node.up = node.header.up # link to nodes in the column
                         node.down = node.header
                         node.header.up.down = node
                         node.header.up = node
+                        node.header.count += 1 # add one to the count of the header
+                        node.row = row * n ** 2 + column * n + value - 1 # set the value of the row
                 else: # if the value is not set, create all the constraints for its position
                     for x in range(n): # loop through all possible values
                         cell = Node(headers[n * row + column]) # nodes for the row-column constraints
@@ -70,9 +95,23 @@ class DLX:
                         for index, node in enumerate(nodes):
                             node.right = nodes[index + 1] # link nodes to each other
                             node.left = nodes[index - 1]
-                            node.header.count += 1 # add one to the count of the header
                             node.up = node.header.up # link to nodes in the column
                             node.down = node.header
                             node.header.up.down = node
                             node.header.up = node
-        return matrix
+                            node.header.count += 1 # add one to the count of the header
+                            node.row = row * n ** 2 + column * n + x # set the value of the row
+    
+    def search(self):
+        if self.root.right == self.root: # if there are no columns left, return the solution
+            return self.solutions
+        header = self.get_minimum()
+        header.cover() # cover the minimum columns
+        vertical_node = header.down
+        while vertical_node != header: # iterate through the rows
+            self.solutions.append(vertical_node)
+            horizontal_node = vertical_node.right
+            while horizontal_node != vertical_node: # iterate through every node in the row and cover the node
+                horizontal_node.header.cover()
+                horizontal_node = horizontal_node.right
+            vertical_node = vertical_node.down
